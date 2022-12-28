a9863ba404abb689483951ed8c45bb60d5bf2207
Matt Rybin <workmateuszrybin@gmail.com>
2022-12-28 06:16:40 +0000

    chore: add storybook

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,3 +1,26 @@
-export const Button: React.FunctionComponent<{ text: string }> = ({ text }) => {
-  return <button>{text}</button>
+// export const Button: React.FunctionComponent<{ text: string }> = ({ text }) => {
+//   return <button>{text}</button>
+// }
+
+interface Props {
+  primary?: boolean;
+  size?: "small" | "large";
+  label?: string;
 }
+ 
+export const Button = ({
+  primary = false,
+  label = "Boop",
+  size = "small",
+}: Props) => {
+  return (
+    <button
+      style={{
+        backgroundColor: primary ? "red" : "blue",
+        fontSize: size === "large" ? "24px" : "14px",
+      }}
+    >
+      WORKS{label}
+    </button>
+  );
+};
