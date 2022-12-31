fb3c13481c12dc13ce0b1bcbff73362f39964d2b
Matt Rybin <workmateuszrybin@gmail.com>
2022-12-31 07:53:16 +0000

    chore udpate

--- .husky/commands/push.sh
+++ .husky/commands/push.sh
@@ -17,6 +17,13 @@ grep -Eo "[0-9]{1,3}")
 # gron | grep title | gron -u | jq -r ".title")
 # echo $ISSUE_TITLE
 
+curl -s \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $CUSTOM_GITHUB_TOKEN"\
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/mattrybin/nextbjj/pulls/8 |
+  gron | grep mergeable_state
+
 # curl \
 #   -H "Accept: application/vnd.github+json" \
 #   -H "Authorization: Bearer $CUSTOM_GITHUB_TOKEN"\
