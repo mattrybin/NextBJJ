a4f86ee6123ab85d80ab0496b2a57255755ce836
Matt Rybin <workmateuszrybin@gmail.com>
2023-01-01 11:27:13 +0000

    dsada

--- .husky/commands/commit.sh
+++ .husky/commands/commit.sh
@@ -2,10 +2,13 @@
 
 echo " "
 echo " "
+echo "Starting Pre-Commit script"
 echo " "
-echo " "
-echo "START"
 SECONDS=0
 pnpm lint && pnpm test:e2e && pnpm test:manypkg
 DURATION_IN_SECONDS=$SECONDS
-echo "\n\n Commit took $DURATION_IN_SECONDSs to run\n\n"
+echo " "
+echo " "
+echo "Commit took $DURATION_IN_SECONDS seconds to run"
+echo " "
+echo " "
