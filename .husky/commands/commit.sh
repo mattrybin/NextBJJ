de8dec0636bf53272182f23802d633f1ccfe6285
Matt Rybin <workmateuszrybin@gmail.com>
2023-01-01 11:25:48 +0000

    dsada

--- .husky/commands/commit.sh
+++ .husky/commands/commit.sh
@@ -8,4 +8,4 @@ echo "START"
 SECONDS=0
 pnpm lint && pnpm test:e2e && pnpm test:manypkg
 DURATION_IN_SECONDS=$SECONDS
-echo $DURATION_IN_SECONDS
+echo "\n\n Commit took $DURATION_IN_SECONDSs to run\n\n"
