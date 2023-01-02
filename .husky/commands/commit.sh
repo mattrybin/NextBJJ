916b3fbb50cfb80963739655d09cdc23555388ec
Matt Rybin <workmateuszrybin@gmail.com>
2023-01-02 19:26:28 +0100

    chore: Complete the pull request flow with finish script (#10)

--- .husky/commands/commit.sh
+++ .husky/commands/commit.sh
@@ -1,3 +1,29 @@
 #!/usr/bin/bash
 
+echo " "
+echo " "
+echo "Starting Pre-Commit script"
+echo " "
+SECONDS=0
+BRANCH_AHEAD=false
 pnpm lint && pnpm test:e2e && pnpm test:manypkg
+DURATION_IN_SECONDS=$SECONDS
+echo " "
+echo " "
+echo "✅ Pre-Commit script took $DURATION_IN_SECONDS seconds to run"
+echo " "
+echo " "
+
+# if [[ -n $(git status -sb | grep "ahead") ]]
+#   then 
+#     git fetch
+#     # [ $(git rev-parse HEAD) = $(git ls-remote $(git rev-parse --abbrev-ref @{u} | \
+#     # sed 's/\// /g') | cut -f1) ] && echo up to date || echo not up to date
+#     # # $(git rev-parse HEAD) == $(git rev-parse @{u})
+#     # # git log HEAD..origin/master --oneline
+#     echo "✅ "
+#   else
+#     BRANCH_AHEAD=false
+#   fi
+# echo " "
+# echo " "
