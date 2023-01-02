4237a9c44208bbbd6e2a6ee76e53d2498edadd09
Matt Rybin <workmateuszrybin@gmail.com>
2023-01-02 15:59:52 +0000

    save

--- .husky/commands/commit.sh
+++ .husky/commands/commit.sh
@@ -5,10 +5,22 @@ echo " "
 echo "Starting Pre-Commit script"
 echo " "
 SECONDS=0
-pnpm lint && pnpm test:e2e && pnpm test:manypkg
+BRANCH_AHEAD=false
+# pnpm lint && pnpm test:e2e && pnpm test:manypkg
 DURATION_IN_SECONDS=$SECONDS
 echo " "
 echo " "
 echo "✅ Pre-Commit script took $DURATION_IN_SECONDS seconds to run"
+if [[ -n $(git status -sb | grep "ahead") ]]
+  then 
+    git fetch
+    [ $(git rev-parse HEAD) = $(git ls-remote $(git rev-parse --abbrev-ref @{u} | \
+    sed 's/\// /g') | cut -f1) ] && echo up to date || echo not up to date
+    # $(git rev-parse HEAD) == $(git rev-parse @{u})
+    # git log HEAD..origin/master --oneline
+    echo "✅ "
+  else
+    BRANCH_AHEAD=false
+  fi
 echo " "
 echo " "
