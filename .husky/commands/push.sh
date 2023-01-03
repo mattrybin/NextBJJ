3bb274ad1ff5d9141ace3cd77cab3e1d6a072acc
Matt Rybin <workmateuszrybin@gmail.com>
2023-01-03 11:39:15 +0000

    fix: husky pre-pull request to merge squash (#13)

--- .husky/commands/push.sh
+++ .husky/commands/push.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/bash
 
 echo -e "===\n>> Pre-push Hook: Checking branch name..."
+# hmm
 
-BRANCH=`git rev-parse --abbrev-ref HEAD`
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
 PROTECTED_BRANCHES="^(master)"
 
-pull_request_status_check () {
+pull_request_status_check() {
   curl -s \
     -H "Accept: application/vnd.github+json" \
     -H "Authorization: Bearer $CUSTOM_GITHUB_TOKEN" \
@@ -14,13 +15,13 @@ pull_request_status_check () {
     gron | grep mergeable_state | grep -q "clean"
 }
 
-pull_request_merge () {
-  curl -X PUT -s  \
+pull_request_merge() {
+  curl -X PUT -s \
     -H "Accept: application/vnd.github+json" \
     -H "Authorization: Bearer $CUSTOM_GITHUB_TOKEN" \
     -H "X-GitHub-Api-Version: 2022-11-28" \
-    -H "merge_method: squash" \
     -H "Content-Type: application/json" \
+    --data-raw '{"merge_method": "squash"}' \
     https://api.github.com/repos/mattrybin/nextbjj/pulls/$1/merge |
     gron | grep merged | grep -q "true"
 }
@@ -29,18 +30,17 @@ pull_request_merge () {
 attempt_counter=0
 max_attempts=200
 
-wait_for_clean_status () {
+wait_for_clean_status() {
   until pull_request_status_check $1; do
-      if [ ${attempt_counter} -eq ${max_attempts} ];
-      then
-        echo "\nMax attempts reached"
-        exit 1
-      fi
+    if [ ${attempt_counter} -eq ${max_attempts} ]; then
+      echo "\nMax attempts reached"
+      exit 1
+    fi
 
-      printf '.'
+    printf '.'
 
-      attempt_counter=$(($attempt_counter+1))
-      sleep 10
+    attempt_counter=$(($attempt_counter + 1))
+    sleep 10
   done
 }
 
@@ -48,19 +48,18 @@ wait_for_clean_status () {
 
 # echo "The branch is clean"
 
-
 # git status -sb | grep "ahead"
 
-ISSUE=$(printenv | 
-grep "CODESPACE_NAME" | 
-grep -Eo "CODESPACE_NAME=mattrybin-[0-9]{1,3}" | 
-grep -Eo "[0-9]{1,3}")
+ISSUE=$(printenv |
+  grep "CODESPACE_NAME" |
+  grep -Eo "CODESPACE_NAME=mattrybin-[0-9]{1,3}" |
+  grep -Eo "[0-9]{1,3}")
 
 ISSUE_TITLE=$(curl -s -X GET "https://api.github.com/repos/mattrybin/nextbjj/issues/${ISSUE}" \
--H 'Accept: application/vnd.github+json' \
--H 'X-GitHub-Api-Version: 2022-11-28' \
--H "Authorization: Bearer $CUSTOM_GITHUB_TOKEN" |
-gron | grep title | gron -u | jq -r ".title")
+  -H 'Accept: application/vnd.github+json' \
+  -H 'X-GitHub-Api-Version: 2022-11-28' \
+  -H "Authorization: Bearer $CUSTOM_GITHUB_TOKEN" |
+  gron | grep title | gron -u | jq -r ".title")
 echo $ISSUE_TITLE
 
 # # curl -s \
@@ -80,15 +79,14 @@ echo $ISSUE_TITLE
 # # echo "{\"issue\": $ISSUE,\"head\":\"issue-$ISSUE\",\"base\":\"master\"}"
 
 # if [[ $(git diff --no-ext-diff --quiet --exit-code) ]]
-#  then 
+#  then
 #   echo "repo is dirty"
 #  else
 #   echo "HELLO"
 # fi
 # # if [[ $(git status --porcelain) ]]; then echo "repo is clean"; fi
 
-if [[ "$BRANCH" =~ $PROTECTED_BRANCHES ]]
-then
+if [[ "$BRANCH" =~ $PROTECTED_BRANCHES ]]; then
   echo -e "\n🚫 Cannot push to remote $BRANCH branch, creating new issue branch ($ISSUE) and PR."
   git reset --soft $(git merge-base HEAD origin/master)
   git commit -am "" --allow-empty-message
@@ -96,12 +94,12 @@ then
   git checkout -t -b "issue-$ISSUE"
   git push -u origin issue-$ISSUE --no-verify
   curl \
-  -X POST \
-  -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer $CUSTOM_GITHUB_TOKEN"\
-  -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/mattrybin/nextbjj/pulls \
-  -d "{\"issue\":$ISSUE,\"head\":\"issue-$ISSUE\",\"base\":\"master\"}"
+    -X POST \
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: Bearer $CUSTOM_GITHUB_TOKEN" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    https://api.github.com/repos/mattrybin/nextbjj/pulls \
+    -d "{\"issue\":$ISSUE,\"head\":\"issue-$ISSUE\",\"base\":\"master\"}"
 
   echo -e "\n⏰ Wait on CI to complete"
   wait_for_clean_status $ISSUE
@@ -109,8 +107,7 @@ then
   exit 0
 else
   echo "HELLO"
-  if [[ -n $(git status -sb | grep "ahead") ]]
-  then 
+  if [[ -n $(git status -sb | grep "ahead") ]]; then
     echo "branch is ahead and we will do normal push"
     exit 0
   else
