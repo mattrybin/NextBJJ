1736bb0bfd0b4551ea779ee1d15a8d49c4d49a5f
Matt Rybin <workmateuszrybin@gmail.com>
2023-01-07 04:12:22 +0000

    save

--- apps/api/src/schema/account.test.ts
+++ apps/api/src/schema/account.test.ts
@@ -2,6 +2,7 @@ import { describe, test, expect} from 'vitest'
 
 export const helloWorld = () => 'Hello World!'
 
+// test
 describe("helloWorld", () => {
 	test("returns 'Hello World!'", () => {
 		expect(helloWorld()).toBe('Hello World!')
