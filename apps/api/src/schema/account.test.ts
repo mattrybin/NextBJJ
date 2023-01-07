5049e256c3645f7ac2d48443cd30bd9b03fc866b
Matt Rybin <workmateuszrybin@gmail.com>
2023-01-07 05:32:07 +0100

    chore: add vitest to web  (#39)

---
+++ apps/api/src/schema/account.test.ts
@@ -0,0 +1,10 @@
+import { describe, test, expect} from 'vitest'
+
+export const helloWorld = () => 'Hello World!'
+
+// test
+describe("helloWorld", () => {
+	test("returns 'Hello World!'", () => {
+		expect(helloWorld()).toBe('Hello World!')
+	})
+})
