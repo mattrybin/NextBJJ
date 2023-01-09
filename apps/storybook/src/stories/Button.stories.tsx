7c4f6cd3d41318e634938bdf0546e682b55d809c
Matt Rybin <workmateuszrybin@gmail.com>
2023-01-09 04:24:14 +0100

    chore: bump all the version in project (#46)

--- apps/storybook/src/stories/Button.stories.tsx
+++ apps/storybook/src/stories/Button.stories.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 // import { Button } from './Button';
-import { Button } from 'ui'
+import { Button } from '@nextbjj/ui'
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
