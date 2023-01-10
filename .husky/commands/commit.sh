9de45ca07ff5d5d820e3a040c3f52d37160c1f5a
Matt Rybin <workmateuszrybin@gmail.com>
2023-01-10 07:36:15 +0000

    save

--- .husky/commands/commit.sh
+++ .husky/commands/commit.sh
@@ -1,30 +1,30 @@
 #!/usr/bin/bash
 
-# pull_request_add_line_numbers () {
-# curl \
-#   -X PATCH \
-#   -H "Accept: application/vnd.github+json" \
-#   -H "Authorization: Bearer $CUSTOM_GITHUB_TOKEN"\
-#   -H "X-GitHub-Api-Version: 2022-11-28" \
-#   https://api.github.com/repos/$OWNER/$REPO/pulls/$1 \
-#   -d '{"title":"new title"}'
-# }
+pull_request_add_line_numbers () {
+curl \
+  -X PATCH \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $CUSTOM_GITHUB_TOKEN"\
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/$OWNER/$REPO/pulls/$1 \
+  -d '{"title":"new title"}'
+}
 
-# pull_request_add_line_numbers 51
+pull_request_add_line_numbers 51
 
-echo " "
-echo " "
-echo "Starting Pre-Commit script"
-echo " "
-SECONDS=0
-BRANCH_AHEAD=false
-pnpm lint && pnpm test:e2e && pnpm test:manypkg
-DURATION_IN_SECONDS=$SECONDS
-echo " "
-echo " "
-echo "✅ Pre-Commit script took $DURATION_IN_SECONDS seconds to run"
-echo " "
-echo " "
+# echo " "
+# echo " "
+# echo "Starting Pre-Commit script"
+# echo " "
+# SECONDS=0
+# BRANCH_AHEAD=false
+# pnpm lint && pnpm test:e2e && pnpm test:manypkg
+# DURATION_IN_SECONDS=$SECONDS
+# echo " "
+# echo " "
+# echo "✅ Pre-Commit script took $DURATION_IN_SECONDS seconds to run"
+# echo " "
+# echo " "
 
 # if [[ -n $(git status -sb | grep "ahead") ]]
 #   then 
