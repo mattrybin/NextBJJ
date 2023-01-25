dac8450625dbbcd7bc727e2d8d08073e8f598653
Matt Rybin <workmateuszrybin@gmail.com>
2023-01-25 15:24:38 +0100

    chore: make the commit e2e fail [+1]  (#81)

--- .husky/commands/commit.sh
+++ .husky/commands/commit.sh
@@ -3,26 +3,27 @@
 SECONDS=0
 AGO=0
 
-function run () {
+function run() {
     AGO=$SECONDS
     printf "🟡 Running $1: "
-    $1 &> /dev/null
+    $1 &>/dev/null
     ret=$?
     if [ $ret = 0 ]; then
-        printf "🟢 completed "  
-        printf "in $(($SECONDS-$AGO))sec \n"
+        printf "🟢 completed "
+        printf "in $(($SECONDS - $AGO))sec \n"
     else
         printf "🔴 Command: $1 failed with exit code: $? "
-        printf "in $(($SECONDS-$AGO))sec \n"
+        printf "in $(($SECONDS - $AGO))sec \n"
+        exit 1
     fi
 }
 
 echo " "
 echo " "
 echo "🔵 Starting commit script"
-run "pnpm lint" 
-run "pnpm test:e2e" 
-run "pnpm test:manypkg" 
+run "pnpm lint"
+run "pnpm test:e2e"
+run "pnpm test:manypkg"
 DURATION_IN_SECONDS=$SECONDS
 echo "🟢 Commit script took $DURATION_IN_SECONDS seconds to run"
 echo " "
