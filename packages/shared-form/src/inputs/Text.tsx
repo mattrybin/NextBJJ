7b8dba49488ccca448fe2257441b406ec1fabb99
Matt Rybin <workmateuszrybin@gmail.com>
2023-01-28 06:10:26 +0000

    ""

--- packages/shared-form/src/inputs/Text.tsx
+++ packages/shared-form/src/inputs/Text.tsx
@@ -6,6 +6,7 @@ const defaultConfig = {
   value: "" as string,
   label: "Label here" as string,
   focused: false,
+  disabled: false,
 }
 
 const machine =
@@ -81,25 +82,28 @@ export const Text = ({ config = {} }: { config?: typeof defaultConfig | {} }) =>
       isEmpty: ({ value }) => value === "",
     },
   })
-  console.log(current.value)
+  const { disabled, focused, placeholder, value, label } = current.context
+  const onFocusedAndNotDisabled = focused && !disabled
+  const onUnfocusedAndNotDisabled = !focused && !disabled
   return (
     <div className="mb-12">
-      <div className="text-slate-700 font-black mb-6">{current.context.label}</div>
+      <div className="text-slate-700 font-black mb-6">{label}</div>
       <div className="relative group">
-        {/* <div className="text-slate-500">{current.context.focused ? "True" : "False"}</div> */}
         <input
-          className="text-slate-700 caret-blue-500 selection:bg-blue-300 focus:shadow-none focus:ring-0 focus:border-2 focus:border-slate-300 relative font-semibold h-15 rounded-md w-full border-2 border-slate-300 placeholder:font-semibold placeholder-slate-400"
+          className={`text-slate-700 caret-blue-500 selection:bg-blue-300 focus:shadow-none focus:ring-0 focus:border-2 focus:border-slate-300 relative font-semibold h-15 rounded-md w-full border-2 border-slate-300 placeholder:font-semibold placeholder-slate-400
+          ${disabled && "bg-slate-100 text-slate-400 cursor-not-allowed	"}`}
           type="text"
+          readOnly={disabled}
           onBlur={() => send({ type: "ON_BLUR", value: false })}
           onFocus={() => send({ type: "ON_FOCUS", value: true })}
           onChange={(e) => send({ type: "ON_CHANGE", value: e.target.value })}
-          value={current.context.value}
-          placeholder={current.context.placeholder}
+          value={value}
+          placeholder={placeholder}
         />
-        {current.context.focused && (
+        {onFocusedAndNotDisabled && (
           <div className="absolute inset-0 -m-6 border-blue-500 border-2 border-solid pointer-events-none rounded-md" />
         )}
-        {!current.context.focused && (
+        {onUnfocusedAndNotDisabled && (
           <div className="absolute inset-0 -m-6 group-focus:border-blue-500 group-focus:border-2 group-hover:border-blue-500 group-hover:border-dashed group-hover:border-2 pointer-events-none rounded-md" />
         )}
       </div>
